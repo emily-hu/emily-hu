2f597b68fe4c9e96be58cf02e19516da9698929b
Emily Hu <emilyhu.mail@gmail.com>
2020-10-11 16:25:05 -0500

    Add header

---
+++ emilyhu/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="content">
+        <div className="header">
+            Hi, I'm Emily!
+        </div>
+        <div>
+            I'm a masters student at MIT, studying computer science.
+        </div>
+        <div>
+            Blah blah i say a lot more here, not really sure what else to include tho.
+        </div>
+    </div>
+  );
+}
+
+export default Header;
