30cd3a427d93237df009e3aece7edd29f36fbf9e
Emily Hu <emilyhu.mail@gmail.com>
2020-10-13 19:42:48 -0500

    Updates

---
+++ precache-manifest.9eb7d379e432c41b4fcaf923468dfcb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38ad3005ab36fbdfc89823676bc03d00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1275d77fb7e90924f76a",
+    "url": "/static/css/main.4b72a7f9.chunk.css"
+  },
+  {
+    "revision": "057b347b8df02ed8288f",
+    "url": "/static/js/2.06f980d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.06f980d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1275d77fb7e90924f76a",
+    "url": "/static/js/main.094e45be.chunk.js"
+  },
+  {
+    "revision": "f65feb8a21079812f6cc",
+    "url": "/static/js/runtime-main.d5427de6.js"
+  },
+  {
+    "revision": "97593a5708f18756bd6c598b67fc8169",
+    "url": "/static/media/xya.97593a57.png"
+  }
+]);
